10ac854c5ba9ec806937f12cf8b85969189ad469
Lalit Chaudhary <lalit3.kumar@paytm.com>
2020-08-06 20:24:50 +0530

    new repo

--- 4.1/Nop.Plugin.Payments.Paytm/Models/PaymentInfoModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Web.Framework.Mvc.ModelBinding;
-using Nop.Web.Framework.Models;
-
-namespace Nop.Plugin.Payments.Paytm.Models
-{
-    public class PaymentInfoModel : BaseNopModel
-    {
-    }
-}
